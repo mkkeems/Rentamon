9849876aa1049ccb12bd4f792cfe700abc4ec475
Michelle Kim <mkkeems@gmail.com>
2020-05-22 14:29:57 +0900

    can see bookings to confirm on my dash

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,4 +4,10 @@ class Booking < ApplicationRecord
 
   validates :start_date, presence: true
   validates :end_date, presence: true
+
+  after_create :set_character_inactive
+  def set_character_inactive
+    self.character.active = false
+    self.character.save
+  end
 end
